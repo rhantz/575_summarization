f40e8812c6bcb37d652b1169ae0d9f20a759f0a0
Yi-Chien Lin <nicayclin@gmail.com>
2023-01-26 22:50:20 -0800

    initialized evaluation script

---
+++ src/run_rouge_eval.py
@@ -0,0 +1,177 @@
+import os
+import argparse
+from sys import argv
+from collections import defaultdict
+from rouge_score import rouge_scorer
+
+individual_results = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(float))))
+total_results = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+
+def collect_summaries(summary_path: str) -> dict:
+    """
+    To collect all summary files and store them in a dict
+    
+    Parameters
+    ----------
+    summary_path: str
+        path to the directory storing all the summary outputs
+    
+    Returns
+    -------
+    summary_dict: dict (defaultdict)
+        a dict storing the summary file names
+        * Format: {"summary_method_id_1": [file1, file2, ...],
+                   "summary_method_id_2": [file7, file10, ...]}
+    
+    """
+    summary_dict = defaultdict(list)
+    summaries = os.listdir(summary_path)
+
+    for summary in summaries:
+        summary_method_id = summary[-1]
+        summary_dict[summary_method_id].append(summary)
+    return summary_dict
+
+def collect_models(model_path: str) -> dict:
+    """
+    To collect all the model files in a designated directory for later use
+
+    Parameters
+    ----------
+    model_path: str
+        path to the directory storing all the models (human summaries)
+    
+    Returns
+    -------
+    model_dict: dict
+        a dict storing the eval id and the corresponding models
+        * Format: {"eval_id_1": [model1, model2, ...], "eval_id_2": [model5, model6, ...]}
+    
+    """
+    model_dict = defaultdict(list)
+    models = os.listdir(model_path)
+
+    for model in models:
+        eval_id = model[:-2]
+        model_dict[eval_id].append(model)
+    return model_dict
+
+def load_data(file_path: str) -> str:
+    """
+    To load data from a specified path
+
+    Parameters
+    ----------
+    file_path: str
+        the path of the file to be opened (str)
+    
+    Returns
+    -------
+    content: str
+        the data of the specified file
+
+    """
+    with open(file_path, 'r', encoding = "utf8") as f:
+        content = f.read()
+    return content
+
+def evaluate_results(summary_dict: dict, model_dict: dict):
+    """
+    TO DO: check
+    Run system evaluation on ROUGE metrics
+    
+    Parameters
+    ----------
+    """
+    rouge_methods = args.rouge_methods
+    for summary_method_id, summary_files in summary_dict.items():
+        for summary_file in summary_files:
+            model_eval_id = summary_file[:-2]
+            summary = load_data(args.summary_path + "/" + summary_file)
+            summary_rouge_scores = defaultdict(lambda: defaultdict(list)) # before taking the average
+            for model_file in model_dict[model_eval_id]:
+                model = load_data(args.model_path + "/" + model_file)
+                scorer = rouge_scorer.RougeScorer(rouge_methods, use_stemmer=True)
+                scores = scorer.score(model, summary)
+
+                for rouge_method in rouge_methods:
+                    precision, recall, fmeasure = scores[rouge_method]
+                    summary_rouge_scores[rouge_method]["P"].append(precision)
+                    summary_rouge_scores[rouge_method]["R"].append(recall)
+                    summary_rouge_scores[rouge_method]["F"].append(fmeasure)
+            
+            avg_p = 0
+            avg_r = 0
+            avg_f = 0
+            for rouge_method, score_type_results in summary_rouge_scores.items():
+                summary_all_p = score_type_results["P"]
+                summary_all_r = score_type_results["R"]
+                summary_all_f = score_type_results["F"]
+                avg_p = round(sum(summary_all_p)/len(summary_all_p), 5)
+                avg_r = round(sum(summary_all_r)/len(summary_all_r), 5)
+                avg_f = round(sum(summary_all_f)/len(summary_all_f), 5)
+
+                individual_results[summary_method_id][rouge_method][summary_file]["P"] = avg_p
+                individual_results[summary_method_id][rouge_method][summary_file]["R"] = avg_r
+                individual_results[summary_method_id][rouge_method][summary_file]["F"] = avg_f
+                
+                total_results[summary_method_id][rouge_method]["P"].append(avg_p)
+                total_results[summary_method_id][rouge_method]["R"].append(avg_r)
+                total_results[summary_method_id][rouge_method]["F"].append(avg_f)
+
+
+    return
+
+def output_eval_results():
+
+
+    return
+
+
+
+
+if __name__ == "__main__":
+
+    # parser = argparse.ArgumentParser(
+    #     description="Run ROUGE evaluation metrics for summarization system.")
+    
+    # parser.add_argument(
+    #     "--summary_path", type=str, required=True, help="path to summmarization system outputs")
+    # parser.add_argument(
+    #     "--model_path", type=str, required=True, help="path to models (gold standard)")
+    # parser.add_argument(
+    #     "--rouge_methods", required=True, help="ROUGE methods used for evaluation (separated with comma)", 
+    #     type=lambda s: [item for item in s.split(',')])
+    # parser.add_argument(
+    #     "--eval_output_path", type=str, required=True, help="path to evaluation output")
+    
+    # args = parser.parse_args(argv[1:])
+    
+    # print(args.summary_path)
+    # print(args.rouge_methods)
+
+
+
+
+
+
+
+    summary_path = "../test/summaries"
+    model_path = "../test/models"
+
+    # summary_dict = collect_summaries(summary_path)
+    # model_dict = collect_models(model_path)
+
+    # print(summary_dict)
+    # print()
+    # print(model_dict)
+
+    summary = load_data("../test/summaries/D1001-A.M.100.A.1")
+    model = load_data("../test/models/D1001-A.M.100.A.H")
+    scorer = rouge_scorer.RougeScorer(['rouge1'], use_stemmer=True)
+    # scores = scorer.score('The quick brown fox jumps over the lazy dog',
+    #                       'The quick brown dog jumps on the log.')
+    scores = scorer.score(model, summary)
+
+    print(scores)
+    # print(load_data("../test/summaries/D1001-A.M.100.A.1"))
