36e4fca312f14c42a89f5d1df8499d64e61035e1
Yi-Chien Lin <nicayclin@gmail.com>
2023-01-27 22:37:21 -0800

    added execution file for D3

---
+++ src/D3_run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate ../env
+
+# Preprocess document
+echo "Preprocessing documents"
+python3 proc_docset.py
+
+# Summarization - tf-idf
+echo "Generating summaries with [tf-idf]"
+
+
+# Summarizarion - ILP
+echo "Generating summaries with [ILP]"
+python3 ilp.py devtest
+
+# Evaluation
+echo "Outputting evaluation results"
+python3 run_rouge_eval.py \
+    --summary_path ../outputs/D3/ \
+    --model_path ~/dropbox/22-23/575x/Data/models/devtest/ \
+    --rouge_methods rouge1,rouge2 \
+    --eval_output_path ../results/D3/ \
+    --eval_output_filename rouge_scores.out
+
